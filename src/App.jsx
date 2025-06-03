6663e64c20d48b868f6ee24e7af1e20bef0d2657
Matias Zurra <matiaszurra@hotmail.com>
2025-06-03 19:59:33 -0300

    Tarea 4 realizada

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   const [unidad, setUnidad] = useState('Celsius');
   const [clima, setClima] = useState('Soleado')
 
+  const resetAll = () => {
+    setUnidad('Celsius');
+    setClima('Soleado')
+  }
+  
+
   const cambiarUnidad = () => {
     if (unidad === "Celsius") {
       setUnidad('Fahrenheit')
@@ -23,7 +29,7 @@ function App() {
   return (
     <div className="container">
       <h1>Estado del tiempo</h1>
-      <Controles onCambiarUnidad={cambiarUnidad} onCambiarClima={cambiarClima} />
+      <Controles onCambiarUnidad={cambiarUnidad} onCambiarClima={cambiarClima} onResetAll={resetAll}/>
       <Display unidad={unidad}  clima={clima} />
     </div>
   )
--- src/components/Controles.jsx
+++ src/components/Controles.jsx
@@ -1,9 +1,10 @@
-const Controles = ({ onCambiarUnidad, onCambiarClima }) => {
+const Controles = ({ onCambiarUnidad, onCambiarClima, onResetAll }) => {
     return (
         <div className='controles-clima'>
             <button onClick={onCambiarUnidad}>Cambiar Unidad</button>
             <button onClick={() => onCambiarClima('Soleado')}>Soleado</button>
             <button onClick={() => onCambiarClima('Nublado')}>Nublado</button>
+            <button onClick={() => onResetAll()}>Reset</button>
         </div>
     )
 }
